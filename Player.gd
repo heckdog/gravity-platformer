ee6bdd7fcfc4dd53340dca853c65164f0cab8953
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2019-04-30 11:39:32 -0400

    upload 1
    this is porbably gonna get scrapped

---
+++ Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const GRAVITY_VEC = Vector2(0, 900)
+const FLOOR_NORMAL = Vector2(0, -1)
+const SLOPE_SLIDE_STOP = 25.0
+const MIN_ONAIR_TIME = 0.1
+const WALK_SPEED = 250 # pixels/sec
+const JUMP_SPEED = 480
+const SIDING_CHANGE_SPEED = 10
+
+var linear_vel = Vector2()
+var onair_time = 0 #
+var on_floor = false
+
+
+
+func _process(delta):
+	#increment counters
+
+	onair_time += delta
+
+	### MOVEMENT ###
+
+	# Apply Gravity
+	linear_vel += delta * GRAVITY_VEC
+	# Move and Slide
+	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
+	# Detect Floor
+	if is_on_floor():
+		onair_time = 0
+
+	on_floor = onair_time < MIN_ONAIR_TIME
+
+	### CONTROL ###
+
+	# Horizontal Movement
+	var target_speed = 0
+	if Input.is_action_pressed("move_left"):
+		target_speed += -1
+	if Input.is_action_pressed("move_right"):
+		target_speed +=  1
+
+	target_speed *= WALK_SPEED
+	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)  # get 10% of the way from linear_vel to target speed
