bc16dcd01913a5535aac866b7ab5afbf20a501a4
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2019-05-01 11:53:29 -0400

    Working Demo
    it just works!
    - gravity switching
    - sounds (flip + impact)
        - also featuring shotty rounding code for impacts. only works for y value changes
    - texture switches

--- Gravity Platformer/Player.gd
+++ Gravity Platformer/Player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var state = "up"
+var impact
+
+var gravity = 2500
+var walkspeed = 350
+var velocity = Vector2()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,4 +25,38 @@ func _process(delta):
 			state = "down"
 		
 		$FlipSound.play()
-		$Label.text = state
+
+# gets inputs
+func get_input():
+	#gravity shift
+	if state == "up":
+		gravity = -2500
+	else:
+		gravity = 2500
+		
+	velocity.x = 0
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	
+	if left:
+		velocity.x += -walkspeed
+	if right:
+		velocity.x += walkspeed
+		
+	
+	
+func _physics_process(delta):
+	var old_posy = round(position.y)  # round due to sub pixel differences
+	
+	# actual physics
+	velocity.y += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	$Label.text = str(old_posy)  # debug
+	# Hit Sound
+	var new_posy = round(position.y)
+	if old_posy != new_posy:
+		if velocity.y == 0:
+			$Impact.play()
+	
