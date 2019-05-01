c772e3e59b8f9e2425a0960b5c7cfaac4c7c37e1
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2019-05-01 10:56:46 -0400

    Initial Upload
    yeet

---
+++ Gravity Platformer/Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var state = "up"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_select"):
+		# flip the animation
+		if $PlayerSprite.animation == "down":
+			$PlayerSprite.animation = "up"
+			state = "up"
+		else:
+			$PlayerSprite.animation = "down"
+			state = "down"
+		
+		$FlipSound.play()
+		$Label.text = state
